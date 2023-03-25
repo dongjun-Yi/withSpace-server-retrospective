cb464ca9635bd3dcd3845eaa3387557937671ef8
dongjun-Yi <djyi0223@gmail.com>
2023-03-25 17:42:20 +0900

    CorsFilter에 Component 애너테이션 추가

--- withSpace/src/main/java/hansung/cse/withSpace/filter/CorsFilter.java
+++ withSpace/src/main/java/hansung/cse/withSpace/filter/CorsFilter.java
@@ -3,9 +3,14 @@ package hansung.cse.withSpace.filter;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
