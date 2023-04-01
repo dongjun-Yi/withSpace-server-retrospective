f5ff143957d1970a812e1b02b7356f60f9435753
hanseul9 <hs990130@naver.com>
2023-04-01 16:51:10 +0900

    시큐리티관련 배포 문제 수정

--- withSpace/src/main/java/hansung/cse/withSpace/config/auth/SecurityConfig.java
+++ withSpace/src/main/java/hansung/cse/withSpace/config/auth/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig{
 //                )
 
                 .authorizeHttpRequests(requests -> requests //로그인 전
-                        .requestMatchers("/", "/home", "/signup", "/member", "/login").permitAll()
+                        .requestMatchers("/", "/home", "/signup", "/member", "/login", "/profile").permitAll()
                         //.requestMatchers("/main").hasRole("USER") //로그인후에는 /main페이지 허용
                         //.anyRequest().permitAll() //임시로 모든 페이지 접근 허용
                         //로그인 안 해도 위 url들은 접근 가능
@@ -75,7 +75,7 @@ public class SecurityConfig{
                         .permitAll()  // 로그인 페이지 이동이 막히면 안되므로 관련된애들 모두 허용
                 )
                 .logout(withDefaults()); // 로그아웃은 기본설정으로 (/logout으로 인증해제)
-                 //.httpBasic();  //테스트 진행을 위해 잠시 설정
+                //.httpBasic();  //테스트 진행을 위해 잠시 설정
 
 
         //.logout((logout) -> logout.permitAll());
