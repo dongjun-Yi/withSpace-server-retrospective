ce0e26173f790ebe0d6bc1228f5ce523d9e7dc7d
dongjun-Yi <djyi0223@gmail.com>
2023-05-04 23:19:45 +0900

    이미지 저장 기능 추가

---
+++ withSpace/src/main/java/hansung/cse/withSpace/controller/FileController.java
@@ -0,0 +1,45 @@
+package hansung.cse.withSpace.controller;
+
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/image")
+@RequiredArgsConstructor
+public class FileController {
+    private final AmazonS3 amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @PostMapping
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = file.getOriginalFilename();
+            String fileUrl = "https://" + bucket + fileName;
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getFile(@RequestParam("file") MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        return ResponseEntity.ok(amazonS3Client.getUrl(bucket, fileName).toString());
+    }
+}
