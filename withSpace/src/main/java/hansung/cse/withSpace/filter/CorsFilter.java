cfe622cf4eedd9bff5ddff825f808c0efaebaa7f
dongjun-Yi <djyi0223@gmail.com>
2023-05-31 01:39:34 +0900

    cors 헤더 설정 수정

--- withSpace/src/main/java/hansung/cse/withSpace/filter/CorsFilter.java
+++ withSpace/src/main/java/hansung/cse/withSpace/filter/CorsFilter.java
@@ -28,7 +28,7 @@ public class CorsFilter implements Filter {
                 "ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, PATCH,REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
+                "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization, jwt-authorization");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
