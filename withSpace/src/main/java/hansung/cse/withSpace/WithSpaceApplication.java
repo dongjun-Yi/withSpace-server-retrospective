4388dfa763933f74c732ddcf839c763b929ac752
dongjun-Yi <djyi0223@gmail.com>
2023-06-01 02:40:06 +0900

    @EnableScheduling 주석처리

--- withSpace/src/main/java/hansung/cse/withSpace/WithSpaceApplication.java
+++ withSpace/src/main/java/hansung/cse/withSpace/WithSpaceApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+//@EnableScheduling
 public class WithSpaceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WithSpaceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WithSpaceApplication.class, args);
+    }
 
 }
